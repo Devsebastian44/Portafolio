56c060f3268cb8ac8a00bd89597598296fc51e2c
Devsebastian44 <zhunaulasebastian31@gmail.com>
2026-01-17 01:09:21 -0500

    Version 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://devsebastian.netlify.app/',
   vite: {
     plugins: [tailwindcss()]
   },
